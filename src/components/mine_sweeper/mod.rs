5449e70bff6dea68d93ce63214d602afba0d85c9
chengchaqiu <chengchaqiu@bytedance.com>
2022-09-22 20:38:24 +0800

    feat: 新增初始化数据

--- src/components/mine_sweeper/mod.rs
+++ src/components/mine_sweeper/mod.rs
@@ -4,7 +4,11 @@ mod styles;
 
 #[function_component(MineSweeper)]
 pub fn mine_sweeper() -> Html {
-  let items = (1..=100).collect::<Vec<i32>>();
+  let mut safe_cells = (1..=90).collect::<Vec<_>>();
+  let mut mine_cells = (1..=10).collect::<Vec<_>>();
+  safe_cells.fill(0);
+  mine_cells.fill(1);
+  safe_cells.append(&mut mine_cells);
 
   // let counter = use_state(|| 0);
 
@@ -20,7 +24,7 @@ pub fn mine_sweeper() -> Html {
 
   html! {
     <div class={styles::container()}>
-      {items.iter().map(|order|html!(<div key={order.to_string()}></div>)).collect::<Html>()}
+      {safe_cells.iter().map(|order|html!(<div key={order.to_string()}>{order}</div>)).collect::<Html>()}
     </div>
   }
 }
