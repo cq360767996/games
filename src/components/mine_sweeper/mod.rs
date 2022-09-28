64c9318eb15a6864189ac522ca65bbfd802de964
chengchaqiu <chengchaqiu@bytedance.com>
2022-09-28 19:21:21 +0800

    feat: 适配新的资源

--- src/components/mine_sweeper/mod.rs
+++ src/components/mine_sweeper/mod.rs
@@ -1,10 +1,16 @@
 use rand::{thread_rng, Rng};
 use uuid::Uuid;
-use yew::{function_component, html, use_state, Callback, Html, MouseEvent};
+use yew::{classes, function_component, html, use_state, Callback, Html, MouseEvent};
+
+#[derive(Clone)]
+enum MineValue {
+  Some(i32),
+  Mine(String),
+}
 
 #[derive(Clone)]
 struct Mine {
-  value: i32,
+  value: MineValue,
   is_open: bool,
   id: String,
 }
@@ -53,7 +59,12 @@ fn gen_mine_cells() -> Vec<Mine> {
       continue;
     }
 
-    for (x, y) in AROUND {
+    let border_arr = pickup_border(&(index as i32));
+
+    for (i, (x, y)) in AROUND.iter().enumerate() {
+      if border_arr.contains(&i) {
+        continue;
+      }
       let current = (index as i32) + y * COLS + x;
       if current < 0 || current >= total {
         continue;
@@ -66,9 +77,19 @@ fn gen_mine_cells() -> Vec<Mine> {
     }
   }
 
+  value_to_cells(&result_cells)
+}
+
+fn value_to_cells(cells: &Vec<i32>) -> Vec<Mine> {
   let mut result = vec![];
-  for value in result_cells {
+  for cell in cells {
     let id = Uuid::new_v4().to_string();
+    let value = if *cell < 9 {
+      MineValue::Some(*cell)
+    } else {
+      MineValue::Mine(String::from("mine"))
+    };
+
     result.push(Mine {
       id,
       value,
@@ -77,14 +98,6 @@ fn gen_mine_cells() -> Vec<Mine> {
   }
 
   result
-}
-
-fn render_text(item: &Mine) -> String {
-  if item.is_open {
-    item.value.to_string()
-  } else {
-    String::from("")
-  }
 }
 
 fn pickup_border(index: &i32) -> Vec<usize> {
@@ -111,29 +124,33 @@ fn open_related_cells<'a>(cells: &'a mut Vec<Mine>, index: &'a usize) {
   }
 
   cells[*index].is_open = true;
-  if cells[*index].value != 0 {
-    return;
+  if let MineValue::Some(0) = cells[*index].value {
+    let border_arr = pickup_border(&(*index as i32));
+
+    for (i, (x, y)) in AROUND.iter().enumerate() {
+      if border_arr.contains(&i) {
+        continue;
+      }
+
+      let current = (*index as i32) + x + y * COLS;
+
+      if current < 0 || current >= total {
+        continue;
+      }
+
+      let current = current as usize;
+      if cells[current].is_open {
+        continue;
+      }
+
+      open_related_cells(cells, &current);
+    }
   }
+}
 
-  let border_arr = pickup_border(&(*index as i32));
-
-  for (i, (x, y)) in AROUND.iter().enumerate() {
-    if border_arr.contains(&i) {
-      continue;
-    }
-
-    let current = (*index as i32) + x + y * COLS;
-
-    if current < 0 || current >= total {
-      continue;
-    }
-
-    let current = current as usize;
-    if cells[current].is_open {
-      continue;
-    }
-
-    open_related_cells(cells, &current);
+fn open_all_cells(cells: &mut Vec<Mine>) {
+  for cell in cells {
+    cell.is_open = true;
   }
 }
 
@@ -148,67 +165,76 @@ pub fn mine_sweeper() -> Html {
     })
   };
 
-  let render_cell = || -> Html {
-    cells
-      .iter()
-      .enumerate()
-      .map(|(index, item)| {
-        let handle_click = {
-          let cells = cells.clone();
-          let ended = (&ended).clone();
+  let render_cell = |(index, item): (usize, &Mine)| {
+    // 点击事件处理
+    let handle_click = {
+      let cells = cells.clone();
+      let ended = (&ended).clone();
 
-          Callback::from(move |_| {
-            let mut new_cells = (*cells).clone();
-            let cell = &mut new_cells[index];
-            if cell.is_open {
-              return;
-            }
-
-            if cell.value < 9 {
-              open_related_cells(&mut new_cells, &index);
-            } else {
-              ended.set(true);
-              return;
-            }
-
-            cells.set(new_cells);
-          })
-        };
-
-        let handle_right_click = {
-          Callback::from(move |e: MouseEvent| {
-            log::info!("right: {}", e.buttons());
-          })
-        };
-
-        html! {
-          <div
-            class="cell opened"
-            key={item.id.clone()}
-            onclick={handle_click}
-            oncontextmenu={handle_right_click}
-            >
-              {render_text(&item)}
-          </div>
+      Callback::from(move |_| {
+        let mut new_cells = (*cells).clone();
+        let cell = &mut new_cells[index];
+        if cell.is_open {
+          return;
         }
+
+        if let MineValue::Mine(_) = cell.value {
+          cell.value = MineValue::Mine("mine_red".to_string());
+          open_all_cells(&mut new_cells);
+          ended.set(true);
+        } else {
+          open_related_cells(&mut new_cells, &index);
+        }
+
+        cells.set(new_cells);
       })
-      .collect::<Html>()
+    };
+
+    let handle_right_click = {
+      Callback::from(move |e: MouseEvent| {
+        log::info!("right: {}", e.buttons());
+      })
+    };
+
+    let get_class_name = || {
+      let mut class_names = vec![];
+      class_names.push("cell".to_string());
+
+      if item.is_open {
+        match &item.value {
+          MineValue::Some(i) => {
+            class_names.push("opened".to_string());
+            class_names.push(format!("type{}", i));
+          }
+          MineValue::Mine(name) => {
+            class_names.push(name.to_string());
+          }
+        }
+      }
+
+      classes!(class_names)
+    };
+
+    html! {
+      <div
+        class={get_class_name()}
+        key={item.id.clone()}
+        onclick={handle_click}
+        oncontextmenu={handle_right_click}
+      />
+    }
   };
 
   html! {
     <>
       <div>
-        {if *ended {
-          String::from("游戏结束")
-        } else {
-          String::from("游戏中")
-        }}
+        {if *ended {"游戏结束"} else {"游戏中"}}
       </div>
       <div
-        class="w-500px h-500px grid grid-cols-10 grid-rows-10"
+        class="w-240px h-240px grid grid-cols-10 grid-rows-10"
         oncontextmenu={handle_contextmenu}
       >
-        {render_cell()}
+        {cells.iter().enumerate().map(render_cell).collect::<Html>()}
       </div>
     </>
   }
